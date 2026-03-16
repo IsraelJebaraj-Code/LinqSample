17b4472e1216e111158d5a9dbd74c4be55763f3c
IsraelJebaraj-Code <israeljebaraj.pla@karpagamtech.ac.in>
2026-03-16 15:35:32 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LinqSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1,2,3,4,5};
+            IList<int> Answer = new List<int>();
+            foreach (int i in list)
+            {
+                if(i %2 == 0)
+                {
+                    Answer.Add(i);
+                }
+            }
+
+            var AnswerEnum = from n in Answer
+                     where n %2 == 0
+                     select n;
+            Answer= AnswerEnum.ToList();
+
+
+
+        }
+    }
+}
